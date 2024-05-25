b59c8ac3d83d0d8d6e2f274088c0b3438a60da19
Danylo Kondratiev <knightpp@proton.me>
2024-05-25 11:14:21 +0300

    Use option to build tests

--- build.zig
+++ build.zig
@@ -37,23 +37,19 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-
-    const build_tests_step = b.step("tests", "Build tests");
-    const tests_artifact = b.addInstallArtifact(lib_unit_tests, .{});
-    build_tests_step.dependOn(&tests_artifact.step);
+    const build_only = b.option(bool, "build", "Build tests but do not run") orelse false;
+    if (build_only) {
+        const tests_artifact = b.addInstallArtifact(lib_unit_tests, .{});
+        test_step.dependOn(&tests_artifact.step);
+    } else {
+        const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+        test_step.dependOn(&run_lib_unit_tests.step);
+    }
 }
