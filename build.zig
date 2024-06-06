e8e440aad3b3028a18ae2cf4d27a62418b786e3b
Danylo Kondratiev <knightpp@proton.me>
2024-06-06 22:58:50 +0300

    Rewrite build.zig

--- build.zig
+++ build.zig
@@ -4,6 +4,24 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const termbox2 = b.dependency("termbox2", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const lib = b.addStaticLibrary(.{
+        .name = "gomodfile",
+        .root_source_file = b.path("src/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.linkLibC();
+    lib.installHeader(termbox2.path("termbox2.h"), "termbox2.h");
+    lib.addCSourceFile(.{ .file = termbox2.path("termbox2.h") });
+
+    const module = b.addModule("gomodfile", .{ .root_source_file = lib.root_module.root_source_file });
+    module.addIncludePath(termbox2.path(""));
+
     const exe = b.addExecutable(.{
         .name = "modupdate",
         .root_source_file = b.path("src/main.zig"),
@@ -12,15 +30,7 @@ pub fn build(b: *std.Build) void {
         .single_threaded = true,
         .strip = b.option(bool, "strip", "strip executable"),
     });
-
-    _ = b.addModule("gomodfile", .{
-        .root_source_file = b.path("src/lib.zig"),
-        .target = target,
-        .optimize = optimize,
-        .single_threaded = true,
-    });
-
-    b.installArtifact(exe);
+    exe.linkLibrary(lib);
 
     const asm_step = b.step("asm", "Produce assembly");
     asm_step.dependOn(&b.addInstallFile(exe.getEmittedAsm(), "modupdate.s").step);
@@ -29,17 +39,12 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_cmd.step);
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
+    lib_unit_tests.linkLibrary(lib);
 
     const test_step = b.step("test", "Run unit tests");
     const build_only = b.option(bool, "build", "Build tests but do not run") orelse false;
@@ -51,17 +56,12 @@ pub fn build(b: *std.Build) void {
         test_step.dependOn(&run_lib_unit_tests.step);
     }
 
-    // b.default_step.dependOn(test_step); // run tests before building
-
     const cov_step = b.step("cov", "Generate coverage");
-
     const cov_run = b.addSystemCommand(&.{ "kcov", "--clean", "--include-pattern=src/", "kcov-output" });
     cov_run.addArtifactArg(lib_unit_tests);
-
     cov_step.dependOn(&cov_run.step);
 
     const lints_step = b.step("lints", "Run lints");
-
     const lints = b.addFmt(.{
         .paths = &.{ "src", "build.zig" },
         .check = true,
@@ -69,4 +69,14 @@ pub fn build(b: *std.Build) void {
 
     lints_step.dependOn(&lints.step);
     b.default_step.dependOn(lints_step);
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // b.default_step.dependOn(test_step); // run tests before building
+
+    b.installArtifact(exe);
 }
